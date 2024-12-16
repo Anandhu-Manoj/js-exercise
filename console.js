79c7a6fea1c1e8c39937ee001f2a4f12ef4c0abc
Anandhu-Manoj <manojanandhu74@gmail.com>
2024-12-16 13:34:40 +0530

    first commit

---
+++ console.js
@@ -0,0 +1,62 @@
+// let z=5
+// let i=1
+// do{
+//     console.log(i*z);
+//     i++
+    
+// }
+// while (i<=10) 
+
+    // sum of n naturalnumbers using for loop
+
+    
+//    let sum=0
+//    let sumn=10
+//    for(let i=1;i<=sumn;i++ ){
+//     sum=sum+i;
+//    }
+//    console.log(`sum of ${sumn}number is ${sum}`);
+   
+
+// // reverse a given number using a while loop
+// let num=12345;
+// let reverse=0;
+
+// while (num>0) {
+//     let digit =num%10;
+//     reverse=reverse*10+digit;
+//     num=Math.floor(num/10)
+// }console.log(reverse);
+
+// count the digits in a Number
+
+
+// let num=12345,count=0;
+
+// do{
+//     count=count+1;
+//     num=Math.floor(num/10)
+// }
+// while (num>0) 
+//     console.log(count);
+    
+
+// find the factorial of number 
+
+// let num=5;
+// let  fact=1;
+// for (let i=1;i<=5;i++){
+//     fact=fact*i
+// }console.log(fact);
+
+    ;
+
+    let num=5
+    let fact=1
+    let i=1
+    while (i<=num) {
+        fact=fact*i;
+        i++
+    }
+    console.log(fact);
+    
